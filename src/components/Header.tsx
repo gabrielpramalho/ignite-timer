4f7a7c86fb9c29f7ff782f29a02a0fd611600250
GABRIEL PEDRO RAMALHO <biel-pedro@hotmail.com>
2023-11-06 22:15:22 -0300

    Add @phosphor-icons/react dependency and update Header component

--- src/components/Header.tsx
+++
@@ -1,7 +0,0 @@
-export function Header() {
-  return (
-    <header>
-      <h1>teste</h1>
-    </header>
-  )
-}
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import { NavLink } from 'react-router-dom'
+import { HeaderContainer } from './styles'
+
+import logoIgnite from '../../assets/logo.svg'
+import { Timer } from '@phosphor-icons/react'
+import { Scroll } from '@phosphor-icons/react/dist/ssr'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={logoIgnite} alt="" />
+      <nav>
+        <NavLink to="/">
+          <Timer size={24} />{' '}
+        </NavLink>
+        <NavLink to="/history">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
+}
