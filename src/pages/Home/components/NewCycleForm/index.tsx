cf3448d16e34e9c5e4e1543cbc1ab99d77b19471
GABRIEL PEDRO RAMALHO <biel-pedro@hotmail.com>
2023-11-10 20:17:13 -0300

    refactored code

---
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -0,0 +1,40 @@
+import { useContext } from 'react'
+import { FormContainer, MinutesAmoutInput, TaskInput } from './style'
+import { CycleContext } from '../..'
+import { useFormContext } from 'react-hook-form'
+
+export function NewCycleForm() {
+  const { activeCycle } = useContext(CycleContext)
+
+  const { register } = useFormContext()
+
+  return (
+    <FormContainer>
+      <label htmlFor="task">Vou trabalhar em</label>
+      <TaskInput
+        id="task"
+        list="task-suggestions"
+        placeholder="Dê um nome para o seu projeto"
+        disabled={!!activeCycle}
+        {...register('task')}
+      />
+
+      <datalist id="task-suggestions">
+        <option value="Projeto 1" />
+      </datalist>
+
+      <label htmlFor="minutesAmount">durante</label>
+      <MinutesAmoutInput
+        type="number"
+        id="minutesAmount"
+        placeholder="00"
+        step={5}
+        min={1}
+        max={60}
+        disabled={!!activeCycle}
+        {...register('minutesAmount', { valueAsNumber: true })}
+      />
+      <span>minutos.</span>
+    </FormContainer>
+  )
+}
